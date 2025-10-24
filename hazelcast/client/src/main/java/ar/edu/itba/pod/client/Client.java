1eb6f31984d5c631c33bd64bc1c32f3a36a4960f
Tomas Pinausig <tpinausigcastillo@itba.edu.ar>
2025-10-24 16:57:42 -0300

    Nueva clase TRIP y modelado de datos de CSV a java

--- hazelcast/client/src/main/java/ar/edu/itba/pod/client/Client.java
+++ hazelcast/client/src/main/java/ar/edu/itba/pod/client/Client.java
@@ -1,18 +1,16 @@
 package ar.edu.itba.pod.client;
 
+import ar.edu.itba.pod.model.Trip;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOError;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Client {
@@ -28,15 +26,22 @@ public class Client {
                 .usePlaintext()
                 .build();
 
+        List<Trip> trips;
         try (Stream<String> lines = Files.lines(Paths.get(CSV_FILE))) {
-            records = lines.map(line -> Arrays.asList(line.split(COMMA_DELIMITER)))
-                    .collect(Collectors.toList());
-        } catch (IOException e){
+            trips = lines
+                    .skip(1)
+                    .map(line -> line.split(";"))
+                    .map(Trip::new)
+                    .toList();
+
+            // DEBUG
+            trips.forEach(t -> {logger.info(t.toString());});
+        } catch(IOException e){
             logger.error(e.getMessage());
         }
 
+
         try {
-            records.forEach(r -> {r.forEach(c -> {logger.info(c);});});
         } finally {
             channel.shutdown().awaitTermination(10, TimeUnit.SECONDS);
         }
