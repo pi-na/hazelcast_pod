57a612e752ffd7f7c1d1f0855c5e3e091d906ef9
kmenshikoff <kmenshikoff@itba.edu.ar>
2025-11-02 16:54:19 -0300

    Add client file

---
+++ hazelcast/client/src/main/java/ar/edu/itba/pod/client/AveragePrice/Client.java
@@ -0,0 +1,88 @@
+package ar.edu.itba.pod.client.AveragePrice;
+
+import ar.edu.itba.pod.api.enums.Params;
+import ar.edu.itba.pod.api.totalTrips.*;
+import ar.edu.itba.pod.client.params.DefaultParams;
+import ar.edu.itba.pod.client.utilities.CsvDataLoader;
+import ar.edu.itba.pod.client.utilities.HazelcastClientFactory;
+import ar.edu.itba.pod.client.utilities.ResultCsvWriter;
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.ICompletableFuture;
+import com.hazelcast.core.IMap;
+import com.hazelcast.mapreduce.Job;
+import com.hazelcast.mapreduce.JobTracker;
+import com.hazelcast.mapreduce.KeyValueSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.concurrent.ExecutionException;
+
+import ar.edu.itba.pod.api.enums.Params;
+import ar.edu.itba.pod.api.totalTrips.*;
+        import ar.edu.itba.pod.client.params.DefaultParams;
+import ar.edu.itba.pod.client.utilities.CsvDataLoader;
+import ar.edu.itba.pod.client.utilities.HazelcastClientFactory;
+import ar.edu.itba.pod.client.utilities.ResultCsvWriter;
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.ICompletableFuture;
+import com.hazelcast.core.IMap;
+import com.hazelcast.mapreduce.Job;
+import com.hazelcast.mapreduce.JobTracker;
+import com.hazelcast.mapreduce.KeyValueSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.*;
+        import java.util.concurrent.ExecutionException;
+
+public class Client {
+
+    private static final String QUERY1_CSV = "query3.csv";
+    private static final String QUERY1_CSV_HEADERS = "pickUpBorough;company;avgFare";
+
+    private static final Logger logger = LoggerFactory.getLogger(ar.edu.itba.pod.client.totalTrips.Client.class);
+
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException{
+        DefaultParams params = DefaultParams.getParams(Params.ADDRESSES.getParam(),
+                Params.INPATH.getParam(),  Params.OUTPATH.getParam());
+        try {
+            HazelcastInstance hazelcastInstance = HazelcastClientFactory.newHazelcastClient(params);
+
+            IMap<String, TotalTrips> iMap = hazelcastInstance.getMap("g5-total-trips");
+
+            System.out.println("🚀 Starting CsvDataLoader...");
+            CsvDataLoader.run(params, true, iMap);
+            System.out.println("✅ CsvDataLoader finished.");
+
+            KeyValueSource<String, TotalTrips> keyValueSource = KeyValueSource.fromMap(iMap);
+
+            JobTracker jobTracker = hazelcastInstance.getJobTracker("g5-total-trips");
+            Job<String, TotalTrips> job = jobTracker.newJob(keyValueSource);
+
+            ICompletableFuture<Map<String, TotalTripsResult>> future = job
+                    .mapper(new TotalTripsMapper())
+                    .combiner(new TotalTripsCombinerFactory())
+                    .reducer(new TotalTripsReducerFactory())
+                    .submit(new TotalTripsCollator());
+
+
+            Map<String, TotalTripsResult> result = future.get();
+            SortedSet<TotalTripsResult> finalResult = new TreeSet<>(
+                    Comparator.comparing(TotalTripsResult::total).reversed()
+                            .thenComparing(TotalTripsResult::pickUpZone)
+                            .thenComparing(TotalTripsResult::dropOffZone));
+
+            ResultCsvWriter.writeCsv(params.getOutPath(), QUERY1_CSV, QUERY1_CSV_HEADERS, finalResult);
+        } finally {
+            HazelcastClient.shutdownAll();
+        }
+    }
+}
